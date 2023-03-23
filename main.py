3e0947cbe0f7d339a78c0a54cc528f2a93556844
tuasonaj <atuason@ufl.edu>
2023-03-22 21:53:08 -0400

    fixed encode and menu function

--- main.py
+++ main.py
@@ -1,37 +1,54 @@
-def encode():
-    password = input('Please enter your password to encode: ')
-    encoded_password = ''
-    for digit in password:
-        encoded_digit = int(digit) + 3
-        encoded_password += str(encoded_digit)
-    print('Your password has been encoded and stored!')
-    print('')
+def encode(password):
+    string_password = str(password)
+    password_digits = [] # creates individual digits
+    encoded_password = ""
 
-def menu():
-    while True:
-        print('Menu')
-        print('-------------')
-        print('1. Encode')
-        print('2. Decode')
-        print('3. Quit')
-        print('')
-        option = int(input("Please enter an option: "))
-        if option == 1:
-            encode()
-        elif option == 3:
-            break
+    for i in range(len(string_password)):
+        password_digits.append(string_password[i])
 
-menu()
+    for i, digit in enumerate(password_digits): # convert back into string
+        password_digits[i] = int(digit)
 
+    for digit in password_digits: # encodes digits
+        if digit in range(0, 7):
+            digit += 3
+            encoded_password += str(digit)
+        elif digit == 7:
+            encoded_password += "0"
+        elif digit == 8:
+            encoded_password += "1"
+        elif digit == 9:
+            encoded_password += "2"
+        elif digit == 0:
+            encoded_password += "3"
 
-
+    return encoded_password
 
 
 
 
+if __name__ == "__main__": # main function
+    stop_menu = False
+    password = 0
+    encoded_password = ""
 
+    while stop_menu is False: # loops menu
+        print("Menu")
+        print("-------------")
+        print("1. Encode")
+        print("2. Decode")
+        print("3. Quit")
+        user_option = int(input("Please enter an option: "))
 
+        if user_option == 1: # encodes password
+            password = str(input("Please enter your password to encode: "))
+            encoded_password = encode(password)
+            print("Your password has been encoded and stored!")
 
-
-
+        elif user_option == 2:
+            decode(encoded_password)
+        elif user_option == 3:
+            stop_menu = True
+            break # ends program
+        print()
 
